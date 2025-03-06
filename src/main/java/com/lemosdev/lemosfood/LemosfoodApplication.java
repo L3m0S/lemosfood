cf5f70076e135b4095ae8517c8631dd613248152
L3m0S <= gabriel_lemos06@hotmail.com>
2025-03-06 17:49:00 -0300

    initial commit

---
+++ src/main/java/com/lemosdev/lemosfood/LemosfoodApplication.java
@@ -0,0 +1,13 @@
+package com.lemosdev.lemosfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemosfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LemosfoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lemosdev/lemosfood/LemosfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lemosdev.lemosfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LemosfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
